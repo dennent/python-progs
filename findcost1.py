154d2c9f3afecf04ff640d5d2ad72f14b0c916d8
dennent <49456943+dennent@users.noreply.github.com>
2019-05-02 09:30:24 -0400

    Add files via upload

---
+++ findcost1.py
@@ -0,0 +1,38 @@
+
+def grd_sh(weight):
+  if weight > 10:
+    return (4.75 * weight) + 20.00
+  elif (weight > 6) or (weight == 10):
+    return (4.00 * weight) + 20.00
+  elif (weight >= 2) or (weight == 6):
+    return (3.00 * weight) + 20.00
+  else:
+    return (1.50 * weight) + 20.00
+    
+    
+
+def dship(weight):
+    if weight >= 10:
+      return weight * 14.25
+    elif (weight >= 6 ) or (weight == 10):
+      return weight * 12.00
+    elif (weight >= 2) or (weight == 6):
+      return weight * 9.00
+    else:
+      return weight * 4.50
+    
+def prm_grd(weight):
+  return 125
+
+def fin_cost(weight):
+  if ((grd_sh(weight)) < (dship(weight))) and ((grd_sh(weight)) < (prm_grd(weight))):
+    return "With a price of, $" + str(grd_sh(weight)) + ", Ground shipping is cheaper than drone and premium ground shipping"
+  elif ((prm_grd(weight)) < (dship(weight))) and ((prm_grd(weight)) < (grd_sh(weight))):
+    return "With a price of, $" + str(prm_grd(weight)) + ", premium ground shipping is cheaper than drone and ground shipping"
+  else:
+    return "With a price of, $" + str(dship(weight)) + ", drone shipping is cheaper than ground and premium ground shipping"
+
+print("To find the cheapest method of shipping type input weight and insert the weight of your package in the corresponding parenthesis")
+while(1):
+	x = input("\ntype weight: ")
+	print(fin_cost(int(x)))
